3201a2c3b1f6d152e269f24af351945543b76c58
Athos Schrapett <athosschrapett@outlook.com>
2021-04-05 10:05:02 -0300

    Finalizado Conteúdo Inicial

---
+++ json/exemplo-01.php
@@ -0,0 +1,15 @@
+<?php
+    $pessoas = array();
+
+    array_push($pessoas, array(
+        'Nome' => 'João',
+        'Idade' => 20
+    ));
+
+    array_push($pessoas, array(
+        'Nome' => 'Glaucio',
+        'Idade' => 25
+    ));
+
+    echo json_encode($pessoas);
+?>
